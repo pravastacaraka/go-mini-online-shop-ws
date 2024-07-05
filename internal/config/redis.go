0397e8646d0283f8ac551ae6bc6a3b27eb892e3d
Pravasta Caraka <proraka@gmail.com>
2024-07-05 14:27:43 +0700

    refactor(env): adjust env config

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -3,21 +3,32 @@ package config
 import (
 	"os"
 	"runtime"
+	"strconv"
 
 	"github.com/gofiber/storage/redis/v3"
 	"github.com/spf13/viper"
 )
 
 func NewRedis(cfg *viper.Viper) *redis.Storage {
-	host := cfg.GetString("database.redis.host")
+	url := ""
+	if os.Getenv("REDIS_URL") != "" {
+		url = os.Getenv("REDIS_URL")
+	}
 
+	host := cfg.GetString("database.redis.host")
 	if os.Getenv("REDIS_HOST") != "" {
 		host = os.Getenv("REDIS_HOST")
 	}
 
+	port := cfg.GetInt("database.redis.port")
+	if os.Getenv("REDIS_PORT") != "" {
+		port, _ = strconv.Atoi(os.Getenv("REDIS_PORT"))
+	}
+
 	store := redis.New(redis.Config{
 		Host:     host,
-		Port:     cfg.GetInt("database.redis.port"),
+		URL:      url,
+		Port:     port,
 		PoolSize: cfg.GetInt("database.redis.pool") * runtime.GOMAXPROCS(0),
 	})
 
